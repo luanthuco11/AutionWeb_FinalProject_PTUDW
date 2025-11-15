5fba54cad650e92d64041c318b7b9964e748c47d
Donavfulish <dvha23@clc.fitus.edu.vn>
2025-11-16 00:55:53 +0700

    feat(page): add BiddingPage

---
+++ client/types/product.ts
@@ -0,0 +1,12 @@
+
+namespace ProductType {
+    export interface BiddingProduct {
+        id: number,
+        main_image: string,
+        name: string,
+        current_price: string,
+        bidding_price: string,
+    }
+}
+
+export default ProductType
