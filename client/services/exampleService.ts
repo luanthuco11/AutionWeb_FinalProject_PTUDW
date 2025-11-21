ac54477cf453bb315830c5466fbc1aa56ac54f42
huynhgiaau27112005 <huynhgiaau27112005@gmail.com>
2025-11-21 16:36:35 +0700

    workspace settings: tabspace 2 for all ts tsx js jsx files

--- client/services/exampleService.ts
+++ client/services/exampleService.ts
@@ -5,26 +5,25 @@ import { api, safeRequest } from "../config/axios.config"
 // NO COUPLING 
 
 export class XService {
+  static async getXBySomething(something: string): Promise<any> {
+    return safeRequest(async () => {
+      const res = await api.get(`/X/${something}`);
+      return res.data
+    });
+  }
 
-    static async getXBySomething(something: string): Promise<any> {
-        return safeRequest(async () => {
-            const res = await api.get(`/X/${something}`);
-            return res.data
-        });
-    }
+  static async createX(payload: any) {
+    return safeRequest(async () => {
+      const res = await api.post('/X', payload);
+      return res.data;
+    });
+  }
 
-    static async createX(payload: any) {
-        return safeRequest(async () => {
-            const res = await api.post('/X', payload);
-            return res.data;
-        });
-    }
-
-    static async updateX(id: string, payload: any): Promise<any> {
-        return safeRequest(async () => {
-            const res = await api.put(`/X/${id}`, payload)
-            return res.data;
-        });
-    }
+  static async updateX(id: string, payload: any): Promise<any> {
+    return safeRequest(async () => {
+      const res = await api.put(`/X/${id}`, payload)
+      return res.data;
+    });
+  }
 }
 
