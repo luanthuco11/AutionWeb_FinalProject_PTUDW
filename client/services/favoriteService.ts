213acc78e001f31ab2ca104380e10092987062a0
huynhgiaau27112005 <huynhgiaau27112005@gmail.com>
2025-11-22 20:44:18 +0700

    feature: apply shared/api into favoriteService + orderService in frontend

--- client/services/favoriteService.ts
+++ client/services/favoriteService.ts
@@ -1,16 +1,17 @@
 import { api, safeRequest } from "../config/axios.config"
+import API_ROUTES from "../../shared/src/api"
 
 export class FavoriteService {
   static async getFavorite(): Promise<any> {
     return safeRequest(async () => {
-      const res = await api.get("/favorite");
+      const res = await api.get(API_ROUTES.favorite.getFavorite);
       return res.data;
     });
   }
 
   static async updateFavorite(productId: number, isFavorite: boolean): Promise<any> {
     return safeRequest(async () => {
-      const res = await api.patch(`/favorite/${productId}/${isFavorite}`);
+      const res = await api.patch(API_ROUTES.favorite.updateFavorite(productId, isFavorite));
       return res.data;
     });
   }
--- client/services/orderService.ts
+++ client/services/orderService.ts
@@ -1,45 +1,45 @@
 import { api, safeRequest } from "@/config/axios.config";
-
+import API_ROUTES from "../../shared/src/api"
 
 export class OrderService {
   static async getOrder(): Promise<any> {
     return safeRequest(async () => {
-      const res = await api.get('/order');
+      const res = await api.get(API_ROUTES.order.getOrder);
       return res.data;
     })
   }
 
   static async getOrderById(productId: number): Promise<any> {
     return safeRequest(async () => {
-      const res = await api.get(`/order/${productId}`);
+      const res = await api.get(API_ROUTES.order.getOrderById(productId));
       return res.data;
     })
   }
 
   static async createOrder(payload: any): Promise<any> {
     return safeRequest(async() => {
-      const res = await api.post(`/order`, payload);
+      const res = await api.post(API_ROUTES.order.createOrder, payload);
       return res.data;
     })
   }
 
   static async updateOrderStatus(productId: number, status: boolean): Promise<any> {
     return safeRequest(async() => {
-      const res = await api.patch(`/order/${productId}/${status}`);
+      const res = await api.patch(API_ROUTES.order.updateOrderStatus(productId, status));
       return res.data;
     })
   }
 
   static async getOrderChat(productId: number): Promise<any> {
     return safeRequest(async() => {
-      const res = await api.get(`/order/${productId}/chat`);
+      const res = await api.get(API_ROUTES.order.getOrderChat(productId));
       return res.data;
     })
   }
 
   static async createOrderChat(productId: number, payload: any): Promise<any> {
     return safeRequest(async() => {
-      const res = await api.post(`/order/${productId}/chat`, payload);
+      const res = await api.post(API_ROUTES.order.createOrderChat(productId), payload);
       return res.data;
     })
   }
