f3776b05eb2896ba984f718169de289c504b5df9
MinhTri27343 <minhtri26072005@gmail.com>
2025-11-29 09:22:59 +0700

    Feat: add sold product api

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -12,169 +12,6 @@ import { BidLog } from "../../shared/src/types";
 import ProductHook from "@/hooks/useProduct";
 import FavoriteHook from "@/hooks/useFavorite";
 
-const mockProductEndTime: Product[] = [
-  {
-    id: 101,
-    slug: "macbook-pro-2022",
-    seller: {
-      id: 2,
-      name: "Trần B",
-      profile_img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQykzoZeCE0p7LeuyHnLYCdPP2jju9d5PaMeA&s",
-    },
-    category_id: 3,
-    main_image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQykzoZeCE0p7LeuyHnLYCdPP2jju9d5PaMeA&s",
-    extra_images: [],
-    name: "MacBook Pro 2022 – M1 Pro 16 inch",
-    initial_price: 25000000,
-    buy_now_price: 35000000,
-    current_price: 28000000,
-    top_bidder: {
-      id: 5,
-      name: "Lê C",
-      profile_img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQykzoZeCE0p7LeuyHnLYCdPP2jju9d5PaMeA&s",
-    },
-    bid_count: 12,
-    end_time: new Date(Date.now() + 4 * 60 * 60 * 1000), // sau 4 giờ
-    description:
-      "MacBook Pro M1 Pro 16 inch, RAM 16GB, SSD 512GB, máy đẹp 99%.",
-    auto_extend: true,
-    price_increment: 500000,
-    created_at: new Date(),
-    updated_at: null,
-  },
-  {
-    id: 102,
-    slug: "macbook-pro-2022",
-    seller: {
-      id: 2,
-      name: "Trần B",
-      profile_img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQykzoZeCE0p7LeuyHnLYCdPP2jju9d5PaMeA&s",
-    },
-    category_id: 3,
-    main_image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQykzoZeCE0p7LeuyHnLYCdPP2jju9d5PaMeA&s",
-    extra_images: [],
-    name: "MacBook Pro 2022 – M1 Pro 16 inch",
-    initial_price: 25000000,
-    buy_now_price: 35000000,
-    current_price: 28000000,
-    top_bidder: {
-      id: 5,
-      name: "Lê C",
-      profile_img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQykzoZeCE0p7LeuyHnLYCdPP2jju9d5PaMeA&s",
-    },
-    bid_count: 12,
-    end_time: new Date(Date.now() + 4 * 60 * 60 * 1000), // sau 4 giờ
-    description:
-      "MacBook Pro M1 Pro 16 inch, RAM 16GB, SSD 512GB, máy đẹp 99%.",
-    auto_extend: true,
-    price_increment: 500000,
-    created_at: new Date(),
-    updated_at: null,
-  },
-  {
-    id: 103,
-    slug: "macbook-pro-2022",
-    seller: {
-      id: 2,
-      name: "Trần B",
-      profile_img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQykzoZeCE0p7LeuyHnLYCdPP2jju9d5PaMeA&s",
-    },
-    category_id: 3,
-    main_image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQykzoZeCE0p7LeuyHnLYCdPP2jju9d5PaMeA&s",
-    extra_images: [],
-    name: "MacBook Pro 2022 – M1 Pro 16 inch",
-    initial_price: 25000000,
-    buy_now_price: 35000000,
-    current_price: 28000000,
-    top_bidder: {
-      id: 5,
-      name: "Lê C",
-      profile_img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQykzoZeCE0p7LeuyHnLYCdPP2jju9d5PaMeA&s",
-    },
-    bid_count: 12,
-    end_time: new Date(Date.now() + 4 * 60 * 60 * 1000), // sau 4 giờ
-    description:
-      "MacBook Pro M1 Pro 16 inch, RAM 16GB, SSD 512GB, máy đẹp 99%.",
-    auto_extend: true,
-    price_increment: 500000,
-    created_at: new Date(),
-    updated_at: null,
-  },
-  {
-    id: 104,
-    slug: "macbook-pro-2022",
-    seller: {
-      id: 2,
-      name: "Trần B",
-      profile_img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQykzoZeCE0p7LeuyHnLYCdPP2jju9d5PaMeA&s",
-    },
-    category_id: 3,
-    main_image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQykzoZeCE0p7LeuyHnLYCdPP2jju9d5PaMeA&s",
-    extra_images: [],
-    name: "MacBook Pro 2022 – M1 Pro 16 inch",
-    initial_price: 25000000,
-    buy_now_price: 35000000,
-    current_price: 28000000,
-    top_bidder: {
-      id: 5,
-      name: "Lê C",
-      profile_img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQykzoZeCE0p7LeuyHnLYCdPP2jju9d5PaMeA&s",
-    },
-    bid_count: 12,
-    end_time: new Date(Date.now() + 4 * 60 * 60 * 1000), // sau 4 giờ
-    description:
-      "MacBook Pro M1 Pro 16 inch, RAM 16GB, SSD 512GB, máy đẹp 99%.",
-    auto_extend: true,
-    price_increment: 500000,
-    created_at: new Date(),
-    updated_at: null,
-  },
-  {
-    id: 105,
-    slug: "macbook-pro-2022",
-    seller: {
-      id: 2,
-      name: "Trần B",
-      profile_img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQykzoZeCE0p7LeuyHnLYCdPP2jju9d5PaMeA&s",
-    },
-    category_id: 3,
-    main_image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQykzoZeCE0p7LeuyHnLYCdPP2jju9d5PaMeA&s",
-    extra_images: [],
-    name: "MacBook Pro 2022 – M1 Pro 16 inch",
-    initial_price: 25000000,
-    buy_now_price: 35000000,
-    current_price: 28000000,
-    top_bidder: {
-      id: 5,
-      name: "Lê C",
-      profile_img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQykzoZeCE0p7LeuyHnLYCdPP2jju9d5PaMeA&s",
-    },
-    bid_count: 12,
-    end_time: new Date(Date.now() + 4 * 60 * 60 * 1000), // sau 4 giờ
-    description:
-      "MacBook Pro M1 Pro 16 inch, RAM 16GB, SSD 512GB, máy đẹp 99%.",
-    auto_extend: true,
-    price_increment: 500000,
-    created_at: new Date(),
-    updated_at: null,
-  }
-];
-
 interface PageItem {
   title: string;
   href?: string;
@@ -202,13 +39,13 @@ const pageItems: PageItem[] = [
 function Page() {
   // const { data: category, isLoading: loa } = CategoryHook.useCategories();
   // const {data, isLoading} = FavoriteHook.useFavorite();
-  const { data, isLoading, error } = ProductHook.useGetProducts();
+  const { data, isLoading, error } = ProductHook.useGetSoldProduct();
 
   // if (isLoading) return <p>Loading...</p>;
   // if (error) return <p>Error!</p>;
 
   if (isLoading) return <p>Loading...</p>;
-
+  console.log(data);
 
   return (
     <>
