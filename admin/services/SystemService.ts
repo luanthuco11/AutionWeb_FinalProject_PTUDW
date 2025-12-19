75382a3141496ef1e78c4a4509553cfea975c745
Donavfulish <dvha23@clc.fitus.edu.vn>
2025-12-19 13:47:32 +0700

    feat(admin-service): add getProductRenewTime service

--- admin/services/SystemService.ts
+++ admin/services/SystemService.ts
@@ -2,12 +2,18 @@ import { api, safeRequest } from "@/config/axios.config";
 import API_ROUTES from "../../shared/src/api";
 
 export class SystemService {
-  static async updateProductRenewTime(time: number): Promise<any> {
-    return safeRequest(async () => {
-      const res = await api.post(API_ROUTES.system.updateProductRenewTime, {
-        time,
-      });
-      return res.data;
-    });
-  }
+    static async getProductRenewTime(): Promise<any> {
+        return safeRequest(async () => {
+            const res = await api.get(API_ROUTES.system.updateProductRenewTime);
+            return res.data;
+        });
+    }
+    static async updateProductRenewTime(time: number): Promise<any> {
+        return safeRequest(async () => {
+            const res = await api.post(API_ROUTES.system.updateProductRenewTime, {
+                time,
+            });
+            return res.data;
+        });
+    }
 }
