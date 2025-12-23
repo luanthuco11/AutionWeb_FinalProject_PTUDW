0195e1dc2dd132351be50894fbb8fa4778f1ccdc
minhluan <luanthuco11@gmail.com>
2025-12-23 20:48:12 +0700

    add : send email

--- server/src/services/ProductService.ts
+++ server/src/services/ProductService.ts
@@ -831,6 +831,17 @@ WHERE pc.parent_id is not null
     userId: number,
     questionId: number
   ) {
+    const getEmailUser = async () => {
+      const sql = `
+      SELECT u.email 
+      FROM product.product_questions as q
+      JOIN admin.users as u ON u.id = q.user_id
+      WHERE q.id = $1 `;
+      const params = [questionId];
+      const result: { email: string }[] = await this.safeQuery(sql, params);
+      return result[0]?.email ?? "";
+    };
+
     const sql = `
     INSERT INTO feedback.product_answers(
     question_id,
@@ -846,6 +857,9 @@ WHERE pc.parent_id is not null
       userId,
       createAnswer.comment,
     ]);
+
+    const emailUser: string = await getEmailUser();
+
     return answer[0];
   }
 
