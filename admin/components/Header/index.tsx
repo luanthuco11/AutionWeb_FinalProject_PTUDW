b2f2a8b6210e4d9e9595d0f66ea716fb92f3d54d
Donavfulish <dvha23@clc.fitus.edu.vn>
2026-01-03 14:44:14 +0700

    chore(admin-component): fix header

--- admin/components/Header/index.tsx
+++ admin/components/Header/index.tsx
@@ -46,7 +46,7 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-white border-b border-gray-200 shadow-sm sticky top-0 z-50">
+    <header className="bg-white border-b border-gray-200 sticky top-0">
       <div className="px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           {/* --- LOGO --- */}
